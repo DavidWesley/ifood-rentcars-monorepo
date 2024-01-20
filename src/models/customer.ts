5768f75196f7d6a14b446cc44ff215e15a6407b9
David Wesley <contato.dwesley@outlook.com>
2024-01-20 20:23:43 -0300

    feat: Add email, birthDate, and gender fields to Customer model

--- src/models/customer.ts
+++ src/models/customer.ts
@@ -2,9 +2,14 @@ import { UUID } from "node:crypto"
 
 import { LicenseType } from "@/models/license.ts"
 
+export type BaseCustomerGenderType = "male" | "female" | "other"
+
 export interface Customer {
     id?: UUID
     name: string
+    email: string
     CPF: string
     license: LicenseType
+    birthDate: Date
+    gender: BaseCustomerGenderType
 }
