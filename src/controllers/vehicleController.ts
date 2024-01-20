b1c93f486be3cf53ec8bf8449ea99ed22726b899
David Wesley <contato.dwesley@outlook.com>
2024-01-19 22:13:26 -0300

    fix: Handle validation and base errors in createVehicle method

--- src/controllers/vehicleController.ts
+++ src/controllers/vehicleController.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express"
 import { ReasonPhrases, StatusCodes } from "http-status-codes"
 
+import { BaseError } from "@/errors/BaseError.ts"
+import { ValidationError } from "@/errors/ValidationError.ts"
 import { createVehicleService } from "@/services/vehicle/CreateVehicleService.ts"
 import { listVehicleService } from "@/services/vehicle/ListVehicleService.ts"
 
@@ -23,22 +25,29 @@ class VehicleController {
 
     public async createVehicle(req: Request, res: Response, next: NextFunction) {
         try {
-            const { plate, type, brand, model, manufacturingYear, color, mass, license, hourlyRentalRate } = req.body
-            const vehicle = await createVehicleService.execute({
-                plate,
-                type,
-                brand,
-                model,
-                manufacturingYear,
-                color,
-                mass,
-                license,
-                hourlyRentalRate,
-            })
+            const vehicle = await createVehicleService.execute(req.body)
             res.status(StatusCodes.CREATED).send(vehicle)
 
             next()
         } catch (err) {
+            if (err instanceof ValidationError) {
+                res.status(err.statusCode).send({
+                    code: err.errorCode,
+                    message: err.message,
+                    errors: err.fieldValidationErrors,
+                })
+            } else if (err instanceof BaseError) {
+                res.status(err.statusCode).send({
+                    name: err.name,
+                    message: err.message,
+                    code: err.errorCode,
+                })
+            } else {
+                res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+                    message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+                })
+            }
+
             next()
         }
     }
