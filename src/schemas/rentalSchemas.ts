416d2360c318f387e9e724eab314c114f2e0c992
David Wesley <contato.dwesley@outlook.com>
2024-01-21 16:04:23 -0300

    feat: Add rentalSchemas module for creating rental body schema

---
+++ src/schemas/rentalSchemas.ts
@@ -0,0 +1,54 @@
+import { z } from "zod"
+
+import { RentalProps } from "@/models/rental.ts"
+import { uuidSchema } from "@/schemas/commons.ts"
+import { TimeUnits } from "@/utils/timeUnits.ts"
+
+export type SettableRentalProperties = Omit<RentalProps, "id" | "returnDate" | "status">
+
+export type SettableRentalPropertiesObjectType = {
+    [key in keyof SettableRentalProperties]: z.ZodType<SettableRentalProperties[key]>
+}
+
+export const createRentalBodySchema = z
+    .object<SettableRentalPropertiesObjectType>({
+        vehicleId: uuidSchema,
+        customerId: uuidSchema,
+        startDate: z.coerce.date().refine(
+            (date) => {
+                const today = new Date()
+                return date <= today
+            },
+            { message: "A data de início deve ser superior ou igual a data atual." }
+        ),
+        endDate: z.coerce.date().refine(
+            (date) => {
+                const today = new Date()
+                return date <= today
+            },
+            { message: "A data de fim deve ser superior ou igual a data atual." }
+        ),
+    })
+    .superRefine((data, ctx) => {
+        const { startDate, endDate } = data
+        if (endDate < startDate) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.invalid_date,
+                message: "A data de fim deve ser superior ou igual a data de início do aluguel",
+                fatal: true,
+                path: ["endDate"],
+            })
+        }
+
+        const diffDurationBetweenDates = endDate.getTime() - startDate.getTime()
+        const diffDurationTimeInHours = TimeUnits.convertTimeDurationToParts(diffDurationBetweenDates, "millisecond", "hour").hour!
+
+        if (diffDurationTimeInHours < 24) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: "A diferença mínima entre as datas de fim e início deve ser de 24 horas",
+                path: ["startDate", "endDate"],
+                fatal: true,
+            })
+        }
+    })
