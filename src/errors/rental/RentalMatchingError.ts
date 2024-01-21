1ca73c876750cff61a35d65d7ccb586f005357c5
Lucas Rocha <lucasgabriels2210@gmail.com>
2024-01-21 14:54:05 -0300

    todo

--- src/errors/rental/RentalMatchingError.ts
+++ src/errors/rental/RentalMatchingError.ts
@@ -5,6 +5,7 @@ import { BaseError } from "@/errors/BaseError.ts"
 export class RentalMatchingFoundError extends BaseError {
     constructor(rentalId:string, customerId: string) {
         const message = `Aluguel com o id:${rentalId} não pertence ao usuário ID: ${customerId}`
+        //TODO definir um erro mais especifico
         super(message, StatusCodes.NOT_FOUND, "NOT_FOUND_ERROR")
         this.name = "RentalMatchingFoundError"
     }
