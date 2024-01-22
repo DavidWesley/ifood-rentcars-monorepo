685543976cf036b6150a266ee75024d16bedf8df
David Wesley <contato.dwesley@outlook.com>
2024-01-21 22:24:30 -0300

    feat(invoice): add CreateInvoiceService functionality and CalculateTotalAmount calculation

--- src/services/invoice/CreateInvoiceService.ts
+++ src/services/invoice/CreateInvoiceService.ts
@@ -1,22 +1,24 @@
-import { Invoice } from "@/models/invoice.ts"
+import { RentalNotFoundError } from "@/errors/rental/RentalNotFoundError.ts"
+import { Invoice, InvoiceStatus } from "@/models/invoice.ts"
 import { Rental } from "@/models/rental.ts"
 import { invoiceRepository } from "@/repositories/invoiceRepository.ts"
 import { rentalRepository } from "@/repositories/rentalRepository.ts"
+import { CalculateTotalAmount } from "@/services/invoice/CalculateTotalAmount.ts"
 
 class CreateInvoiceService {
-    public async execute(rentalId: Rental["id"], hourlyRate: number, startDate: Date, endDate: Date): Promise<Required<Invoice>> {
+    public async execute(rentalId: NonNullable<Rental["id"]>): Promise<Required<Invoice>> {
         const rental = await rentalRepository.findById(rentalId)
         if (rental === null) {
-            throw new Error("Aluguel não encontrado.")
+            throw new RentalNotFoundError(rentalId)
         }
 
-        const diffHours = Math.abs(endDate.getTime() - startDate.getTime()) / 3600000;
-        const totalCost = diffHours * hourlyRate;
+        const totalAmount = await CalculateTotalAmount.calculate(rental)
 
-        const invoice = new Invoice(rental.id!, totalCost)
-        await invoiceRepository.create(invoice)
-
-        return invoice
+        return await invoiceRepository.create({
+            rentalId: rental.id,
+            status: InvoiceStatus.Pending,
+            totalCost: totalAmount,
+        })
     }
 }
 
