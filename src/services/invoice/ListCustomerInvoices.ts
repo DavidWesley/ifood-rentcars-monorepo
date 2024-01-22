d734de143d2e663c27aeae2c18c6d66b46b5c7a2
David Wesley <contato.dwesley@outlook.com>
2024-01-21 22:30:32 -0300

    feat: Add ListCustomerInvoices service for retrieving customer invoices

---
+++ src/services/invoice/ListCustomerInvoices.ts
@@ -0,0 +1,19 @@
+import { CustomerNotFoundError } from "@/errors/customer/CustomerNotFoundError.ts"
+import { Customer } from "@/models/customer.ts"
+import { Invoice } from "@/models/invoice.ts"
+import { customerRepository } from "@/repositories/customerRepository.ts"
+import { invoiceRepository } from "@/repositories/invoiceRepository.ts"
+
+class ListCustomerInvoices {
+    public async execute(cpf: Customer["CPF"], state?: Invoice["status"]): Promise<Required<Invoice>[]> {
+        const customer = await customerRepository.findByCPF(cpf)
+        if (customer === null) {
+            throw new CustomerNotFoundError(cpf)
+        }
+
+        const invoices = await invoiceRepository.findAllByCustomerId(customer.id)
+        return state ? invoices.filter((invoice) => invoice.status === state) : invoices
+    }
+}
+
+export const listCustomerInvoices = new ListCustomerInvoices()
