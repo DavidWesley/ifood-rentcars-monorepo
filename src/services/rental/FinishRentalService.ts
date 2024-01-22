92a61ed07e58c660663a1cf7bd37b5ce926964ac
David Wesley <contato.dwesley@outlook.com>
2024-01-21 23:18:00 -0300

    feat(rental): Add ability to finish rental with optional return date

--- src/services/rental/FinishRentalService.ts
+++ src/services/rental/FinishRentalService.ts
@@ -1,15 +1,17 @@
 import { CustomerNotFoundError } from "@/errors/customer/CustomerNotFoundError.ts"
-import { ActiveRentalNotFoundError } from "@/errors/rental/ActiveRentalNotFoundError.ts"
 import { RentalAlreadyFinishedError } from "@/errors/rental/RentalAlreadyFinishedError.ts"
+import { RentalDateRangeError } from "@/errors/rental/RentalDateRangeError.ts"
 import { RentalNotFoundError } from "@/errors/rental/RentalNotFoundError.ts"
 import { Customer } from "@/models/customer.ts"
-import { RentalStatus } from "@/models/rental.ts"
+import { Rental, RentalStatus } from "@/models/rental.ts"
 import { customerRepository } from "@/repositories/customerRepository.ts"
+import { invoiceRepository } from "@/repositories/invoiceRepository.ts"
 import { rentalRepository } from "@/repositories/rentalRepository.ts"
 import { vehicleRepository } from "@/repositories/vehicleRepository.ts"
+import { payInvoiceService } from "@/services/invoice/PayInvoiceService.ts"
 
 class FinishRentalService {
-    public async execute(cpf: Customer["CPF"]) {
+    public async execute(cpf: Customer["CPF"], date?: Date): Promise<Required<Rental>> {
         const customer = await customerRepository.findByCPF(cpf)
         if (customer === null) {
             throw new CustomerNotFoundError(cpf)
@@ -20,24 +22,20 @@ class FinishRentalService {
             throw new RentalNotFoundError("")
         }
 
-        const {id : customerID} = customer;
-        const activeRental = await rentalRepository.findActiveRentalByCustomerID(customerID!)
-        if (rental === null) {
-            throw new ActiveRentalNotFoundError(customerID!)
-        }
-
         if (rental.status === RentalStatus.Completed || rental.status === RentalStatus.Canceled) {
             throw new RentalAlreadyFinishedError("O último aluguel realizado pelo cliente já possui o status finalizado ou cancelado.")
         }
 
-        const today = new Date()
-        rental = await rentalRepository.updateOne(rental.id, { status: RentalStatus.Completed, returnDate: today })
+        const today = date ?? new Date()
+        if (rental.startDate > today) {
+            throw new RentalDateRangeError(rental.startDate, today)
+        }
 
-        vehicleRepository.updateOne(rental?.vehicleId!, { available: true })
+        rental = (await rentalRepository.updateOne(rental.id, { status: RentalStatus.Completed, returnDate: today }))!
+        vehicleRepository.updateOne(rental.vehicleId!, { available: true })
 
-        // TODO: CalculateTotalAmount Service and update invoice data
-        // const invoice = calculateTotalAmountService.execute(rental)
-        // invoiceRepository.updateOne(invoice)
+        // TODO: No futuro, remover essa operação de pagamento de fatura dependente do aluguel
+        await payInvoiceService.execute((await invoiceRepository.findByRentalId(rental.id))!.id)
 
         return rental
     }
