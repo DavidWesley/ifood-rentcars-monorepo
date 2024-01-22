99f38698f644a6533fbcc79a8ac735251499f622
David Wesley <contato.dwesley@outlook.com>
2024-01-21 22:18:41 -0300

    feat: Add findByRentalId and findAllByCustomerId methods in invoiceRepository

--- src/repositories/invoiceRepository.ts
+++ src/repositories/invoiceRepository.ts
@@ -1,6 +1,9 @@
 import { randomUUID } from "node:crypto"
 
+import { Customer } from "@/models/customer.ts"
 import { Invoice } from "@/models/invoice.ts"
+import { Rental } from "@/models/rental.ts"
+import { rentalRepository } from "@/repositories/rentalRepository.ts"
 
 class InvoiceRepository {
     private static data: Required<Invoice>[] = []
@@ -25,6 +28,25 @@ class InvoiceRepository {
         }
 
         return InvoiceRepository.data[invoiceIndex]!
+    }
+
+    public async findByRentalId(rentalId: NonNullable<Rental["id"]>): Promise<Required<Invoice> | null> {
+        const invoice = InvoiceRepository.data.find((invoice) => invoice.rentalId === rentalId)
+        return invoice ?? null
+    }
+
+    public async findAllByCustomerId(customerId: NonNullable<Customer["id"]>): Promise<Required<Invoice>[]> {
+        const rentals = await rentalRepository.findAllByCustomerId(customerId)
+        const invoices: Required<Invoice>[] = []
+
+        for await (const rental of rentals) {
+            const invoice = await invoiceRepository.findByRentalId(rental.id)
+            if (invoice !== null) {
+                invoices.push(invoice)
+            }
+        }
+
+        return invoices
     }
 }
 
