a7a664cc7fead03f7f1d8a83817b07f76c1eb182
David Wesley <contato.dwesley@outlook.com>
2024-01-31 19:46:06 -0300

    feat(env): Add Node version check and exit if not supported

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,13 @@
 import { z } from "zod"
 
-import { env } from "node:process"
+import { env, versions } from "node:process"
+
+const [major, minor] = versions.node.split(".", 2).map((value) => Number.parseInt(value, 10)) as [number, number]
+
+if (Boolean((major >= 20 && minor >= 11) || major >= 21) === false) {
+    console.error("This program must be executed with Node >= v20.11.1")
+    process.exit(1)
+}
 
 const envSchema = z.object({
     NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
