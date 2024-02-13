5edd96729e3a896dc2e88e46844eb906c47cd4df
David Wesley <contato.dwesley@outlook.com>
2024-02-13 16:06:31 -0300

    refactor: Update return types of select and list methods

--- src/repositories/customerRepository.ts
+++ src/repositories/customerRepository.ts
@@ -3,46 +3,9 @@ import { randomUUID } from "node:crypto"
 import { Customer } from "@/models/customer.ts"
 
 class CustomerRepository {
-    protected static data: Required<Customer>[] = [
-        {
-            id: "d8a892f5-571d-4bd3-b44d-498441cce918",
-            name: "João da Silva",
-            email: "joao@example.com",
-            CPF: "12442272008",
-            birthDate: new Date("2001-02-04"),
-            license: "A",
-            gender: "male",
-        },
-        {
-            id: "cdab164a-fa1c-48f7-80f2-782c67d12547",
-            name: "John Doe",
-            email: "john.doe@example.com",
-            CPF: "61305705033",
-            license: "A",
-            birthDate: new Date("1990-01-01"),
-            gender: "male",
-        },
-        {
-            id: "d9a3af3e-1372-4d2e-8ab5-cb8602738195",
-            name: "Jane Doe",
-            email: "jane.doe@example.com",
-            CPF: "26171836028",
-            license: "B",
-            birthDate: new Date("1995-05-15"),
-            gender: "female",
-        },
-        {
-            id: "7222abd5-ba68-437d-a706-5aa4a7352dc1",
-            name: "Sam Smith",
-            email: "sam.smith@example.com",
-            CPF: "89291776033",
-            license: "AB",
-            birthDate: new Date("1985-08-20"),
-            gender: "other",
-        },
-    ]
+    protected static data: Required<Customer>[] = []
 
-    public async select(filter: Partial<Customer>): Promise<Customer[]> {
+    public async select(filter: Partial<Customer>): Promise<Required<Customer>[]> {
         const filterEntries = Object.entries(filter)
         if (filterEntries.length === 0) {
             return this.list()
@@ -53,13 +16,13 @@ class CustomerRepository {
         })
     }
 
-    public async list(): Promise<Customer[]> {
+    public async list(): Promise<Required<Customer>[]> {
         return Array.from(CustomerRepository.data)
     }
 
     public async add(props: Omit<Customer, "id">): Promise<Required<Customer>> {
         const size = await CustomerRepository.data.push({ id: randomUUID(), ...props })
-        return CustomerRepository.data[size - 1]!
+        return CustomerRepository.data[size - 1] as Required<Customer>
     }
 
     public async findById(id: NonNullable<Customer["id"]>): Promise<Required<Customer> | null> {
