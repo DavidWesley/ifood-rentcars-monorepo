03eff8b988643335462940dcf7a0066c5c962513
David Wesley <contato.dwesley@outlook.com>
2024-02-22 21:36:44 -0300

    refactor: Simplify error handling in ErrorHandlerMiddleware and LogMiddleware

--- src/routes/middlewares/LogMiddleware.ts
+++ src/routes/middlewares/LogMiddleware.ts
@@ -1,5 +1,6 @@
 import { ENV } from "@repo/env"
 import { NextFunction, Request, Response } from "express"
+import { RequestPropertiesNameType } from "./ValidateRequestSchemaMiddleware.ts"
 
 export class LogMiddleware {
     private static DATE_FORMATTER = new Intl.DateTimeFormat("pt-BR", {
@@ -8,34 +9,35 @@ export class LogMiddleware {
         timeZone: "America/Sao_Paulo",
     })
 
-    private static JSON_FORMATTER = (value: object) => {
+    private static JSON_FORMATTER = (value: object | null) => {
         return JSON.stringify(value, null, 2)
     }
 
-    public static handle(req: Request, res: Response, next: NextFunction) {
-        const isEmpty = (object: object) => {
-            if (!object) return true
-            return Object.keys(object).length === 0
+    public static handle(req: Request, _: Response, next: NextFunction) {
+        const isEmpty = (object: object | null) => {
+            if (object === null) return true
+            return Reflect.ownKeys(object).length === 0
         }
 
         const now = new Date()
         const dateTimeString = LogMiddleware.DATE_FORMATTER.format(now)
 
-        const requestInfoObject = {
-            params: isEmpty(req.params) ? {} : req.params,
-            query: isEmpty(req.query) ? {} : req.query,
-            body: isEmpty(req.body) ? {} : req.body,
-        }
+        const requestInfoTupleArray: Array<[RequestPropertiesNameType, object | null]> = [
+            ["params", req.params],
+            ["query", req.query],
+            ["body", req.body],
+        ]
 
-        const requestInfo = LogMiddleware.JSON_FORMATTER(requestInfoObject)
+        const requestInfoObject = requestInfoTupleArray.filter(([_, obj]) => !isEmpty(obj))
+        const requestInfoMessage = LogMiddleware.JSON_FORMATTER(requestInfoObject)
 
         switch (ENV.NODE_ENV) {
             case "production":
-                console.log(`[LOG] ${dateTimeString} ${req.method} ${req.originalUrl}`)
+                console.log("[LOG] %s %s %s", dateTimeString, req.method, req.originalUrl)
                 break
             case "development":
-                console.group(`[LOG] ${dateTimeString} ${req.method} ${req.url}`)
-                if (requestInfo !== "") console.log(requestInfo)
+                console.log("[LOG] %s %s %s", dateTimeString, req.method, req.url)
+                if (requestInfoObject.length > 0) console.log(requestInfoMessage)
                 console.groupEnd()
                 break
             case "test":
