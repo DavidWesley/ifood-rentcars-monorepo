a8ab12f6726ceb25815ee6676192d570d582506f
David Wesley <contato.dwesley@outlook.com>
2024-02-23 21:31:29 -0300

    refactor: Update ENV object initialization to use SERVER_ENV definition

--- packages/env/main.ts
+++ packages/env/main.ts
@@ -1,20 +1,7 @@
-import { createEnv } from "@t3-oss/env-nextjs"
-import { z } from "zod"
+import { SERVER_ENV } from "./schema/server.ts"
 
-const ENV = createEnv({
-    server: {
-        NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
-        PORT: z.coerce.number().positive().min(80).max(65_000).default(3333),
-        HOST: z.string().ip().default("127.0.0.1"),
-        DATABASE_URL: z.string().url(),
-    },
-    runtimeEnv: {
-        NODE_ENV: process.env.NODE_ENV,
-        DATABASE_URL: process.env.DATABASE_URL,
-        HOST: process.env.HOST,
-        PORT: process.env.PORT,
-    },
-    emptyStringAsUndefined: true,
-})
+const ENV = {
+    ...SERVER_ENV,
+}
 
 export { ENV }
